58f197fbbeb96bde4ffd6bd451d8f2e6f0932bbe
tomas_dev <97204775+tomasquinteros@users.noreply.github.com>
2023-08-08 15:06:10 -0300

    Desafio terminado

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const ulElement = document.getElementsByClassName('lista')
+  const liElements = document.getElementsByTagName('li')
+  Array.from(liElements).forEach(list => {
+    ulElement[0].removeChild(list)
+  })
+  const newList = () => {
+    const array = []
+    for (let i=0; i < cosasQueAprendimos.length; i++) {
+      const li = document.createElement('li')
+      li.textContent = cosasQueAprendimos[i].tema
+      array.push(li)
+    }
+    return array
+  }
+  ulElement[0].append(...newList())
+}
 
 main();
